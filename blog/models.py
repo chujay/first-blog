34c30f11f12ae6d9bda7b496c1f84b33cf0bfb00
Chujay <Chujay@Chujayde-MacBook-Pro.local>
2017-09-05 08:56:58 +0800

    2017090509

--- blog/models.py
+++ blog/models.py
@@ -1,22 +1,21 @@
 
 from django.db import models
 from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
 
+@python_2_unicode_compatible
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-
-def publish(self):
-    """docstring for publish"""
-    self.published_date = timezone.now()
-    self.save()
-
-def __self__(self):
-    """docstring for __self__"""
-    return self.title
-    
+    def publish(self):
+        """docstring for publish"""
+        self.published_date = timezone.now()
+        self.save()
+    def __str__(self):
+        """docstring for __self__"""
+        return self.title
 
 
